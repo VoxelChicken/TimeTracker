5898c8511d4103be4d706ed0add885541041a576
VoxelChicken <251214636+VoxelChicken@users.noreply.github.com>
2026-03-03 21:37:50 +0100

    TimeTracker Class Overhaul This update includes changes like: - Class methods - Comments and documentation (mostly documentation)

--- code/TimeTracker.py
+++ code/TimeTracker.py
@@ -1,4 +1,4 @@
-import os
+import os, time, sys
 
 
 def modify_file(target_file, data):
@@ -14,23 +14,41 @@ class TimeTracker:
     This class is there to easily write a TimeTracker (or modify it, since it exists anyway).
     """
 
-    def __init__(self, program_to_check: str, program_to_run: str):
+    def __init__(self, program_to_check: str, program_to_run: str, target_file: str):
         self.error_msg = ""
         self.program_to_check = program_to_check
 
         modify_file("program_to_check", program_to_check)
         modify_file("program_to_run", program_to_run)
 
-    def save_usage_time(self, time_before_start: float, time_at_start, time_at_end: float, target_file):
+        self.target_file = target_file
+
+        self.time_at_start = time.time()
+        self.time_already_used_prior = float(open(self.target_file, "r").readline())
+
+    def get_time_run_until_now(self):
+        """
+        This method gets the current time spent in the program. Since the program then does NOT close automatically, you can use it without worry.
+        :return: float
+        """
+        current_time = time.time()
+        time_run_until_now = current_time - self.time_at_start
+
+        return current_time
+
+    def save_usage_time(self, end_program: bool):
         """
         This function stores the time at the start and end of the program and then calculates the amount of time spent in it.
         That then gets added to the current amount that is already stored in the file.
         """
-        if not os.path.exists(target_file):
-            cmd = "touch " + target_file
+        if not os.path.exists(self.target_file):
+            cmd = "touch " + self.target_file
             os.system(cmd)
 
-        time_used = time_at_end - time_at_start
+        time_used_until_now = self.get_time_run_until_now() - self.time_at_start
 
-        file = open(target_file, "w")
-        file.write(str(time_used + time_before_start))
+        file = open(self.target_file, "w")
+        file.write(str(time_used_until_now + self.time_already_used_prior))
+
+        if end_program:
+            sys.exit("Program stopped via the TimeTracker class method 'save_usage_time'.")
--- code/main.py
+++ code/main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import TimeTracker, time, atexit, os
+import TimeTracker, os, atexit
 
 
 def run(script: str):
@@ -13,13 +13,8 @@ def run(script: str):
     os.system(script)
 
 
-tc = TimeTracker.TimeTracker(program_to_check="godot-bin", program_to_run="org.godotengine.Godot")
-
-TIME_SPENT_PRIOR = float(open("seconds-data", "r").readline())
-TIME_AT_START = time.time()
+tc = TimeTracker.TimeTracker(program_to_check="godot-bin", program_to_run="org.godotengine.Godot", target_file="seconds-data")
 
 run("./run_program.sh")
 
-TIME_AT_END = time.time()
-
-atexit.register(tc.save_usage_time, time_before_start=TIME_SPENT_PRIOR, time_at_start=TIME_AT_START, time_at_end=TIME_AT_END, target_file="seconds-data")
+atexit.register(tc.save_usage_time, end_program=True)
